19fd6cf73813311c2efc83cb27cb78a619523c19
Ethan <ethanrickert0429@gmail.com>
2020-11-06 22:22:18 -0600

    Core files
    LootLord.cs - Patches ExecuteTroop function
    SubModuleLoad.cs - Applies 0Harmony patch(es)

---
+++ LootLord.cs
@@ -0,0 +1,29 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using HarmonyLib;
+
+namespace LootLord
+{
+    [HarmonyPatch(typeof(PartyScreenLogic), "ExecuteTroop")]
+    public class PartyScreenLogic_LootLord
+    {
+        public static void Postfix(PartyScreenLogic.PartyCommand command)
+        {
+            CharacterObject character = command.Character;
+            Hero hero = character.HeroObject;
+            PartyBase mainParty = PartyBase.MainParty;
+
+            for (int i = 0; i < 12; ++i)
+            {
+                if (hero.BattleEquipment[i].IsEmpty) { continue; }
+                if (hero.BattleEquipment[i].Item.ItemCategory.IsAnimal) { continue; }
+                mainParty.ItemRoster.AddToCounts(hero.BattleEquipment[i].Item, 1, true);
+                InformationManager.DisplayMessage(new InformationMessage(string.Concat(new object[]
+                {
+                    hero.BattleEquipment[i].Item.ToString(),
+                    " Added"
+                })));
+            }
+        }
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using TaleWorlds.DotNet;
+using TaleWorlds.MountAndBlade;
+
+namespace LootLord
+{
+    public class SubModule : MBSubModuleBase
+    {
+        public static string ErrorFile;
+
+        protected override void OnSubModuleLoad()
+        {
+            try
+            {
+                base.OnSubModuleLoad();
+                new Harmony("LootLord").PatchAll();
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    string text = string.Format("Error-{0:yyyy-MM-dd-HH-mm-ss}.txt", DateTime.Now);
+                    SubModule.ErrorFile = text;
+                    string arg_85_0 = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), text);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.AppendLine("Installed Modules:");
+                    stringBuilder.AppendLine(Managed.GetModulesVersionStr());
+                    stringBuilder.AppendLine("Exception Message:");
+                    stringBuilder.AppendLine(ex.ToString());
+                    File.WriteAllText(arg_85_0, stringBuilder.ToString());
+                }
+                catch
+                {
+                }
+            }
+        }
+    }
+}
